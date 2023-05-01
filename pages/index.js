f943f6129edb5c6d8408da1a3d8b2c97a039210a
DORTROX プロ <levisaustin011@gmail.com>
2023-05-01 18:24:39 +0100

    SSSA

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Islogged from "@/components/Islogged";
 import TaskCard from "@/components/TaskCards";
 import { getSession } from "next-auth/react";
 import { useUserContext } from "@/context/UserContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { checkAndSave } from "@/Firebase/UserInit";
 import { OnTaskComplete, Task } from "@/Firebase/TaskInit";
 
@@ -11,32 +11,29 @@ export default function Home() {
   const urls = ["https://tii.la/"];
 
   useEffect(() => {
-    return () => {
-      getSession().then(async (session) => {
-        if (urls.includes(document.referrer)) {
-          const now = new Date();
-          await OnTaskComplete(session.user.id, now);
-        } else {
-          // Do nothing like me
-        }
-        if (session == null) return;
-        const user = await checkAndSave(session.user);
-        const tasks = await Task();
-        setTask(tasks);
-        await setUser({
-          discordId: user.discordId,
-          email: user.email,
-          username: user.username,
-          profile: session.user.image,
-          coin: user.coin,
-          isAuth: true,
-          isAdmin: user.admin,
-          CompletedTasks: user.Task,
-          isCompleting: user.isCompleting,
-        });
-
+    getSession().then(async (session) => {
+      if (urls.includes(document.referrer)) {
+        const now = new Date();
+        await OnTaskComplete(session.user.id, now);
+      } else {
+        // Do nothing
+      }
+      if (session == null) return;
+      const user = await checkAndSave(session.user);
+      const tasks = await Task();
+      setTask(tasks);
+      setUser({
+        discordId: user.discordId,
+        email: user.email,
+        username: user.username,
+        profile: session.user.image,
+        coin: user.coin,
+        isAuth: true,
+        isAdmin: user.admin,
+        CompletedTasks: user.Task,
+        isCompleting: user.isCompleting,
       });
-    };
+    });
   }, [setTask, setUser]);
   return (
     <>
