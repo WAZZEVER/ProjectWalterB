cd3b71295f28d9d01d053f9266d3002d5b8acb87
WAZZEVER <101801884+WAZZEVER@users.noreply.github.com>
2023-05-06 17:58:03 +0530

    Update config.js

--- Firebase/config.js
+++ Firebase/config.js
@@ -1,17 +1,17 @@
 const { initializeApp } = require("firebase/app");
 const { collection, getFirestore } = require("firebase/firestore");
 const firebaseConfig = {
-  apiKey: "AIzaSyA5cabZFDFcLMl6TNARDfT92t0PTc2dC8o",
-  authDomain: "mallory-2981a.firebaseapp.com",
-  projectId: "mallory-2981a",
-  storageBucket: "mallory-2981a.appspot.com",
-  messagingSenderId: "375464208367",
-  appId: "1:375464208367:web:af289418749b9071ab781a",
-  measurementId: "G-ZQVGY9NXP9",
+  apiKey: "AIzaSyDbCzXFmL3Hyex2uzwiYRaWNvPCWJLudDU",
+  authDomain: "testwazz-460a6.firebaseapp.com",
+  projectId: "testwazz-460a6",
+  storageBucket: "testwazz-460a6.appspot.com",
+  messagingSenderId: "226425030359",
+  appId: "1:226425030359:web:82ba3348579b4ee5ab9292",
+  measurementId: "G-X7XC55K4VE"
 };
 const Gapp = initializeApp(firebaseConfig);
 const db = getFirestore(Gapp);
-const discordAuthCol = collection(db, "walterB");
+const discordAuthCol = collection(db, "Wazzever");
 const offerCol = collection(db, "Task");
 
 module.exports = { discordAuthCol, offerCol, db };
