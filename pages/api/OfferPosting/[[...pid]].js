e6d31785c067adb2f415ead8299417f84dda32ed
WAZZEVER <101801884+WAZZEVER@users.noreply.github.com>
2023-05-07 12:53:58 +0800

    Update [[...pid]].js

--- pages/api/OfferPosting/[[...pid]].js
+++ pages/api/OfferPosting/[[...pid]].js
@@ -6,7 +6,7 @@ export default function Page(req, res) {
   const name = req.query.pid[1]
   const price = parseFloat(req.query.pid[2])
   const cooldown = parseInt(req.query.pid[3])
-  const url = `https://shrinkearn.com/api?api=${process.env.SHRINKEARN}&url=${req.query.pid[0]}`;
+  const url = `https://shrinkearn.com/api?api=213fef8892c6f7d480e9cc3af072e5e1c999d485&url=${req.query.pid[0]}`;
   fetch(url).then(resp => resp.json())
   .then( async (data) => {
     const x = await CreateTask(cooldown, name, price, data.shortenedUrl)
