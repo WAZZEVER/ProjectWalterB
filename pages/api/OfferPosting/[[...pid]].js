47c897aeeadfbc7c235217fbc34d691e908144c7
WAZZEVER <101801884+WAZZEVER@users.noreply.github.com>
2023-05-08 16:51:52 +0530

    Update [[...pid]].js

--- pages/api/OfferPosting/[[...pid]].js
+++ pages/api/OfferPosting/[[...pid]].js
@@ -2,9 +2,10 @@ import { CreateTask } from '@/Firebase/TaskInit';
 
 export const Page = async(req, res) => {
   const dest= req.query.pid[0]
-  const name = req.query.pid[1]
-  const price = parseFloat(req.query.pid[2])
-  const cooldown = parseInt(req.query.pid[3])
+  const dest2 = req.query.pid[1]
+  const name = req.query.pid[2]
+  const price = parseFloat(req.query.pid[3])
+  const cooldown = parseInt(req.query.pid[4])
   // const link = req.query.pid[4];
   // let url;
   // switch (link) {
@@ -13,6 +14,6 @@ export const Page = async(req, res) => {
   //   case "shrinkearn":
   //     url = `https://shrinkearn.com/api?api=213fef8892c6f7d480e9cc3af072e5e1c999d485&url=${dest}`;
   // }
-    const x = await CreateTask(cooldown, name, price, dest)
+    const x = await CreateTask(cooldown, name, price, dest+ "/"+dest2)
     res.send(200)
 }
