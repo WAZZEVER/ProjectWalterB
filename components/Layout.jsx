69aa26cc44eae0f8c5b648ecf7f6883f71352f67
WAZZEVER <101801884+WAZZEVER@users.noreply.github.com>
2023-05-08 21:26:47 +0800

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,13 @@ const Layout = ({ children }) => {
     <div className='main-layout'>
       <Head>
         <title>WAZZEVER</title>
-        <link rel="icon" href="https://th.bing.com/th/id/OIP.MsbTJ1aP-6jw_lY6BHGh0AHaEK?pid=ImgDet&rs=1" />
+<link rel="icon" href="https://cdn.discordapp.com/attachments/875195544010633306/1027553675100569600/WAZZ_3.png" type="image/png">
+<style>
+  /* Create a circle with a 50% border radius */
+  head > link[rel="icon"] {
+    border-radius: 50%;
+  }
+</style>
       </Head>
       <header>
         <Navbar />
