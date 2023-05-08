5f66da53a22dd80aa6a6e7afadf7041cbe7e626e
WAZZEVER <101801884+WAZZEVER@users.noreply.github.com>
2023-05-08 16:46:18 +0530

    Update [[...pid]].js

--- pages/api/OfferPosting/[[...pid]].js
+++ pages/api/OfferPosting/[[...pid]].js
@@ -1,15 +1,18 @@
 import { CreateTask } from '@/Firebase/TaskInit';
-import fetch from 'node-fetch';
 
-export default function Page(req, res) {
+export const Page = async(req, res) => {
   const dest= req.query.pid[0]
   const name = req.query.pid[1]
   const price = parseFloat(req.query.pid[2])
   const cooldown = parseInt(req.query.pid[3])
-  const url = `https://shrinkearn.com/api?api=213fef8892c6f7d480e9cc3af072e5e1c999d485&url=${req.query.pid[0]}`;
-  fetch(url).then(resp => resp.json())
-  .then( async (data) => {
-    const x = await CreateTask(cooldown, name, price, data.shortenedUrl)
-res.status(200)
-  })
+  // const link = req.query.pid[4];
+  // let url;
+  // switch (link) {
+  //   case "shrinkme":
+  //     url = `https://shrinkme.com/api?api=b46c551c8f63bb760d69ac67cb1ed1175cc586c6&url=${dest}`;
+  //   case "shrinkearn":
+  //     url = `https://shrinkearn.com/api?api=213fef8892c6f7d480e9cc3af072e5e1c999d485&url=${dest}`;
+  // }
+    const x = await CreateTask(cooldown, name, price, dest)
+    res.send(200)
 }
