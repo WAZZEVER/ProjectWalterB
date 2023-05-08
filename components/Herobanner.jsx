2e9060a70be5c557ca6efecd379857e6446e7ff3
WAZZEVER <101801884+WAZZEVER@users.noreply.github.com>
2023-05-08 21:55:13 +0800

    Update Herobanner.jsx

--- components/Herobanner.jsx
+++ components/Herobanner.jsx
@@ -22,32 +22,30 @@ export default function HeroBanner() {
         justify={'center'}
         px={useBreakpointValue({ base: 4, md: 8 })}
         bgGradient={'linear(to-r, blackAlpha.600, transparent)'}>
-<Stack maxW={'2xl'} align={'center'} align={'flex-start'} spacing={6}>
-  <Text
-    color={'white'}
-    fontWeight={700}
-    lineHeight={1.2}
-    fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}>
-    Complete Task To earn
-  </Text>
-  <Stack direction={'row'} gap={5}>
-    <Button
-      bg={'blue.400'}
-      rounded={'full'}
-      color={'white'}
-      _hover={{ bg: 'blue.500' }}>
-      Learn More
-    </Button>
-    <Button
-      bg={'whiteAlpha.300'}
-      rounded={'full'}
-      color={'white'}
-      _hover={{ bg: 'whiteAlpha.500' }}>
-      Start Earning
-    </Button>
-  </Stack>
-</Stack>
-
+        <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
+          <Text
+            color={'white'}
+            fontWeight={700}
+            lineHeight={1.2}
+            fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}>
+            Complete Task To earn
+          </Text>
+          <Stack direction={'row'} gap={5}>
+            <Button
+              bg={'blue.400'}
+              rounded={'full'}
+              color={'white'}
+              _hover={{ bg: 'blue.500' }}>
+              Learn More
+            </Button>
+            <Button
+              bg={'whiteAlpha.300'}
+              rounded={'full'}
+              color={'white'}
+              _hover={{ bg: 'whiteAlpha.500' }}>
+              Start Earning
+            </Button>
+          </Stack>
       </VStack>
     </Flex>
   );
